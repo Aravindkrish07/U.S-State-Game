43a8ede52afbed2c034313b76ae551eca6b4d6b3
Aravindkrish07 <49242734+Aravindkrish07@users.noreply.github.com>
2024-03-18 23:08:30 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S State Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_state = data.state.to_list()
+guessed_state = []
+
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 States Correct", prompt="what's another state's name?").title()
+    if answer_state == "Exit":
+        missing_state = []
+        for i in all_state:
+            if i not in guessed_state:
+                missing_state.append(i)
+        new_data = pandas.DataFrame(missing_state)
+        new_data.to_csv("states_to_learn")
+        break
+    if answer_state in all_state:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
